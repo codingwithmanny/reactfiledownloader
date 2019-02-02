510761d952d763dfe60a841609ad9633a4a77ac9
m <clkadlaaslk@remotecode.local>
2019-02-01 19:02:11 -0500

    Fix: Added refactoring to remove logs and add comments

--- src/helpers.js
+++ src/helpers.js
@@ -1,36 +1,45 @@
 const Helpers = {
-     httpRequest: (url, method, payload, headers) => {
-          const config = {
-               method,
-               headers: {
-                    'Content-Type': 'application/json'
-               }
-          };
+  // Main wrapper for Fetch API
+  httpRequest: (url, method, payload, headers) => {
 
-          if (payload && payload.length > 0) {
-               config.body = JSON.stringify(payload);
-          }
+    // Configuration to accept json as a default
+    const config = {
+      method,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
 
-          if (headers && typeof headers === 'object' & Object.keys(headers).length > 0) {
-               config.headers = headers;
-          }
+    // method = post and payload, add it to the fetch request
+    if (method.toLowerCase() === 'post' &&  payload && payload.length > 0) {
+      config.body = JSON.stringify(payload);
+    }
 
-          return fetch(
-               url,
-               config
-           ).then((response) => {
-               if (response.ok) {
-                    let data = response;
-                    if (response.headers.get('Content-Type').indexOf('application/json') > -1) {
-                    data = response.json();
-                    }
-                    console.log('first then');
-                    console.log(response.headers.get('Content-Type'));
-                    return data;
-               }
-               return Promise.reject(response);
-           });
-     },  
+    // if custom headers need to be set for the specific request
+    // override them here
+    if (headers && typeof headers === 'object' && Object.keys(headers).length > 0) {
+      config.headers = headers;
+    }
+
+    return fetch(
+      url,
+      config
+    ).then((response) => {
+      // Check if the request is 200
+      if (response.ok) {
+        let data = response;
+        
+        // if the type is json return, interpret it as json
+        if (response.headers.get('Content-Type').indexOf('application/json') > -1) {
+          data = response.json();
+        }
+        return data;
+      }
+
+      // if an errors, anything but 200 then reject with the actual response
+      return Promise.reject(response);
+    });
+  },  
 };
 
 export default Helpers;
